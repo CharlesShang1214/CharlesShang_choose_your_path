23fa2dd062e6d8ac94d95d29e1f2c8ab045aab66
CharlesShang1214 <student.shang27@student.tamdistrict.org>
2024-10-22 09:59:06 -0700

    more choices made

--- uno38projectChooseYourPath.java
+++ uno38projectChooseYourPath.java
@@ -5,15 +5,56 @@ public class uno38projectChooseYourPath {
     public static void main(String[] main) {
         Scanner sc = new Scanner(System.in);
         boolean ta = true;
+        int choice1 = 0;
+        int choice2 = 0;
+        int choice3 = 0;
         while (ta) {
             boolean life = true;
             while (life) {
                 System.out.println(
-                        "You wake up on a sandy beach. You are extremely tried and you lost most of your memory.");
+                        "You wake up on a sandy beach. You have nothing on you and you lost most of your memory.");
                 System.out.println("You wanna go home");
                 System.out.println("What would you choose?");
-                System.out.println("1: ");
-                int choice1 = getGuess(3);
+                System.out.println("1: Take another nap.");
+                System.out.println("2: Walk around");
+
+                choice1 = getGuess(2);
+                if (choice1 == 1) {
+                    System.out.println(
+                            "You get the best nap in your entire life, this nap is so comfortable you will never awake again.");
+                    life = false;
+                }
+                if (choice1 == 2) {
+                    System.out.println("You decided to walk around");
+                    System.out.println("After a couple minutes of wandering, you get super hungry and thristy");
+                    System.out.println("Luckyly, there is some food trees infront of you");
+                    System.out.println("Which one would you choose?");
+                    System.out.println("1: Coconut Tree");
+                    System.out.println("2: Watermelon Tree");
+                    System.out.println("3: Ice Tree");
+                    choice2 = getGuess(3);
+
+                    if (choice2 == 1) {
+                        System.out.println("Since Coconut is your favourite food, you decided to get some coconut");
+                        System.out.println("What would you do?");
+                        System.out.println("1: Climb the tree");
+                        System.out.println("2: Shake the tree");
+                        choice3 = getGuess(2);
+                    }
+                    if (choice2 == 2) {
+                        System.out.println("Since strawberry is your favourite food, you decided to get some chicken");
+
+                        System.out.println("What would you do?");
+                        System.out.println("1: Climb the tree");
+                        System.out.println("2: Shake the tree");
+                    }
+                    if (choice2 == 3) {
+                        System.out.println("Since Ice is your favourite food, you decided to get some Ice");
+                        System.out.println("What would you do?");
+                        System.out.println("1: Climb the tree");
+                        System.out.println("2: Shake the tree");
+                    }
+                }
 
             }
             System.out.println("Wanna try again?(yes or no all lower case)");
